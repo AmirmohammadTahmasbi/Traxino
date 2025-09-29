bdce2be1eea7ef5d1e83900339093c8e04c4606a
Amirmohammad Tahmasbi <139885266+AmirmohammadTahmasbi@users.noreply.github.com>
2025-09-29 20:34:22 +0330

    feat: add Persian font and configure Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        yekanbakh: ["YekanBakh", "sans-serif"],
+      },
+      colors: {
+        primary: "#ff3333",
+        primaryGray: "#ff3333ae",
+        black: "#323232",
+        backGround: "#EDF3F9",
+      }
+    },
   },
   plugins: [],
-}
-
+};
