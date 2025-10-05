a0a60382da645360334481d3ac9620817251c26c
Amirmohammad Tahmasbi <139885266+AmirmohammadTahmasbi@users.noreply.github.com>
2025-10-05 11:25:00 +0330

    Add header section and update Tailwind config colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
         yekanbakh: ["YekanBakh", "sans-serif"],
       },
       colors: {
-        primary: "#ff3333",
-        primaryGray: "#ff3333ae",
-        black: "#323232",
-        backGround: "#EDF3F9",
-      }
+        primary: "#1F2937", // تیره، برای هدر و بک‌گراند اصلی
+        secondary: "#F97316", // آبی روشن، لینک‌ها و CTAها
+        accent: "#3B82F6", // نارنجی/طلایی برای تاکید و هور
+        background: "#111827", // بک‌گراند کل سایت
+        textPrimary: "#F9FAFB", // متن اصلی
+        textSecondary: "#9CA3AF", // متن ثانویه
+      },
     },
   },
   plugins: [],
